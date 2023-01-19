acc90a5c773a9868b30ce98ad1cac97e67df25b1
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-19 16:41:08 +0300

    passed all test

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,17 +8,26 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+  function handleDeleteClick() {
+    onDeleteClick(id);
+  }
 
+  function handleAnswerChange(event) {
+    onAnswerChange(id, parseInt(event.target.value));
+  }
   return (
+   
     <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
+    <h4>Question {id}</h4>
+    <h5>Prompt: {prompt}</h5>
+    <label>
+      Correct Answer:
+      <select defaultValue={correctIndex} onChange={handleAnswerChange}>
+        {options}
+      </select>
+    </label>
+    <button onClick={handleDeleteClick}>Delete Question</button>
+  </li>
   );
 }
 
